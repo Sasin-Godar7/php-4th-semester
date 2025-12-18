e2b4908bbf7305987d84129175e3a6d7fe977210
Sasin Godar <sago_bca2080@lict.edu.np>
2025-12-18 12:42:53 +0545

    .

---
+++ std_register.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <fieldset>
+            <label for="name">
+                <input type="text" name="name" require>
+            </label>
+            <label for="roll">
+                <input type="num" name="roll" require>
+            </label>
+            <label for="gender">
+                <input type="radio" name="gender" value="male">Male
+                <input type="radio" name="gender" value="female">Female
+               </label>
+               <select name="Course" id="course" disabled="disabled">
+                <option value="BCA">bca</option>
+                <option value="BCA">bca</option>
+                <option value="BCA">bca</option>
+                <option value="BCA">bca</option>
+                <option value="BCA">bca</option>
+               </select>
+
+               
+   
+
+        </fieldset>
+    </form>
+    
+</body>
+</html>
